1bf333bb18c633caf8908644bcb240f7c3abbc5c
Smelly Shrimp <tymek.lamza@op.pl>
2025-02-17 22:59:47 +0100

    Added .draw() function to View implementation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 struct View {
-    buff: [[char; 24]; 80],
+    buff: [[char; 80]; 24],
     height: u8,
 }
 
+impl View {
+    fn draw(&self) {
+        for row in self.buff.iter() {
+            for &elm in row.iter() {
+                print!("{elm}");
+            }
+
+            print!("\n")
+        }
+    }
+}
+
 fn main() {
-    let mut foo = View {
-        buff: [[' '; 24]; 80],
+    let foo = View {
+        buff: [['.'; 80]; 24],
         height: 20,
     };
+
+    foo.draw();
 }

49d9c6ffe788eb57661b822074b4d50d63db2439
Smelly Shrimp <tymek.lamza@op.pl>
2025-02-17 23:54:24 +0100

    Added .get_chunk()

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use rand::Rng;
+
 mod terminal;
 
 struct View {
     buff: [[char; 80]; 24],
-    height: u8,
+    height: usize,
 }
 
 impl View {
@@ -20,13 +22,30 @@ impl View {
         terminal::wait_for_exit();
         terminal::exit_alter_scr_buff();
     }
+
+    fn get_chunk(&self) -> [char; 24] {
+        let mut res = ['.'; 24];
+
+        let max_val = res.len() - self.height;
+
+        let mut h = rand::rng().random_range(0..=max_val);
+
+        h = res.len() - (self.height + h);
+
+        for i in h..res.len() {
+            res[i] = '#';
+        }
+
+        res
+    }
 }
 
 fn main() {
     let foo = View {
         buff: [['.'; 80]; 24],
-        height: 20,
+        height: 8,
     };
 
     foo.draw();
+    println!("{:?}", foo.get_chunk());
 }
